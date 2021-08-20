3e227649cb9fe5fe7c21f1fe2dd2c1d5b933305d
Chris Dalziel <christopher_dalziel@hotmail.com>
2021-08-20 10:53:56 +1000

    movies response created

--- src/main/kotlin/to/dev/example/api/responses/MoviesResponse.kt
+++ src/main/kotlin/to/dev/example/api/responses/MoviesResponse.kt
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.annotation.JsonPropertyDescription
 import com.fasterxml.jackson.databind.annotation.JsonSerialize
 import com.kjetland.jackson.jsonSchema.annotations.JsonSchemaDescription
 import com.kjetland.jackson.jsonSchema.annotations.JsonSchemaTitle
+import net.ozwolf.raml.annotations.RamlExample
 
 @JsonSerialize
 @JsonSchemaTitle("Movies Response")
@@ -12,5 +13,32 @@ import com.kjetland.jackson.jsonSchema.annotations.JsonSchemaTitle
 data class MoviesResponse (
     @get:JsonProperty(value = "provider", required = true)
     @get:JsonPropertyDescription("provider")
-    val provider: String
-)
+    val provider: String,
+
+    @get:JsonProperty(value = "movies", required = true)
+    @get:JsonPropertyDescription("list of movies")
+    val movies: Movies
+) {
+    companion object {
+        @JvmStatic
+        @RamlExample
+        fun example(): MoviesResponse = MoviesResponse(
+            "provider",
+            Movies("ID", "title", "type", "poster")
+        )
+    }
+}
+
+data class Movies (
+    @get:JsonProperty(value = "ID", required = true)
+    var ID: String,
+
+    @get:JsonProperty(value = "title", required = true)
+    var title: String,
+
+    @get:JsonProperty(value = "type", required = true)
+    var type: String,
+
+    @get:JsonProperty(value = "poster", required = true)
+    var poster: String
+    )
